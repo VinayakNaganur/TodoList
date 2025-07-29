8c0ff45cd0f1918e2b1067ab8c56f6f5da5e3bb3
VinayakNaganur <vinayaknaganur37@gmail.com>
2025-07-30 00:27:20 +0530

    todo list completed

--- Todoapp/src/main/java/com/app/Todoapp/models/Task.java
+++ Todoapp/src/main/java/com/app/Todoapp/models/Task.java
@@ -5,13 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Entity
 @Data
-@AllArgsConstructor
+
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
