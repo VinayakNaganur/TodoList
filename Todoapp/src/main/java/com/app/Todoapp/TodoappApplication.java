58490cd2d38251f18a12f6fd8163daba9c264e7c
VinayakNaganur <vinayaknaganur37@gmail.com>
2025-07-29 21:46:57 +0530

    jj

---
+++ Todoapp/src/main/java/com/app/Todoapp/TodoappApplication.java
@@ -0,0 +1,15 @@
+package com.app.Todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world \n \n ");
+		System.out.println("vinayak naganur");
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+}
