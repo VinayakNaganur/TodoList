7c0365f39147c0572c23d30627facae6943ad67f
VinayakNaganur <vinayaknaganur37@gmail.com>
2025-07-29 22:15:31 +0530

    jj

---
+++ Todoapp/src/main/java/com/app/Todoapp/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.app.Todoapp.repository;
+
+import com.app.Todoapp.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository <Task,Long> {
+
+}
